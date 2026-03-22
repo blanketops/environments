1793065b25b8e755ab81e9f7bbd5b6330f6a24f3
Neo Tlaletsi <ntlaletsi86@gmail.com>
2026-03-22 23:45:15 +0200

    bugfix(staickcheck):, update static check, add config, add ptr as required, staticheck through

--- pkg/packages/api/application.go
+++ pkg/packages/api/application.go
@@ -12,6 +12,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
 	"k8s.io/client-go/tools/events"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
